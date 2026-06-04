d731449f4f1a1072979bff691c157f493052bc21
LekhaBollineni <lekhabollineni2022@gmail.com>
2026-06-04 15:24:36 -0400

    "hello world"

--- 1_hello.py
+++ 1_hello.py
@@ -1,5 +1,8 @@
 def say_hello(name):
+    """This program says hello world"""
     print(f"Hello, this is {name}")
 
 if __name__ == "__main__":
+    print(say_hello.__doc__)
     say_hello("Lekha")
+
